f5b133e6b8aa5bcd181b58566b4920c9a2fadc52
theandrew168 <steveday168@gmail.com>
2018-08-21 21:14:16 -0500

    Add a few simple models

--- timelogger/models.py
+++ timelogger/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+    city = models.CharField(max_length=500)
+    county = models.CharField(max_length=500)
+
+
+class Visit(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    visit_date = models.DateTimeField()
+    duration = models.IntegerField()
